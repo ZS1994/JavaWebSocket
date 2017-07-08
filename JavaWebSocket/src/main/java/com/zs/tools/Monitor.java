f91a4561d099df3066767d48ac7de62517e4cff4
ZS1994 <1217360619@qq.com>
2017-07-08 15:22:07 +0800

    1132

---
+++ JavaWebSocket/src/main/java/com/zs/tools/Monitor.java
@@ -0,0 +1,28 @@
+package com.zs.tools;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import com.zs.websocket.WebSocketTest;
+
+
+public class Monitor implements Runnable {
+
+	WebSocketTest webSocketTest = new WebSocketTest();
+	SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+	
+	public void run() {
+        webSocketTest.sendMsg("当前时间:" + sdf.format(new Date()));
+    }
+
+    public void sendMsg() {
+        ScheduledExecutorService newScheduledThreadPool = Executors.newSingleThreadScheduledExecutor();
+        newScheduledThreadPool.scheduleWithFixedDelay(new Monitor(), 20, 5, TimeUnit.SECONDS);
+
+    }
+}
+
